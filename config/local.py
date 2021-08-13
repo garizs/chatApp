e913c851dd19d212d12995c1de6bb7df6f7bb1ae
Ivan Kolesnikov <i.kolesnikov@ritg.ru>
2021-08-13 10:34:42 +0400

    Initial commit

---
+++ config/local.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_test',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    },
+}
